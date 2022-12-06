8e65618022efcc690e369f531e5d7b30278c8957
Jayden6849 <jayden6849@gmail.com>
2022-12-06 14:14:16 +0900

    study for java: ch01

---
+++ _ch01/src/Hotkey.java
@@ -0,0 +1,28 @@
+
+public class Hotkey {
+
+	public static void main(String[] args) {
+		
+		// Ctrl + Shift + L : 단축키 전체 목록보기
+		// Ctrl + +,- : 폰트 크기 증가, 감소
+		// Ctrl + D : 한 줄 삭제
+		// Ctrl + Alt + Down : 행단위 복사
+		//		>> Ctrl + Alt + Shift + Down 으로 변경
+		//				Window - Preferences - General - Keys - Copy Lines
+		// Alt + Shift + A : 멀티 컬럼 편집 (*마우스 커서가 십자모양으로 바뀜)
+		// Alt + Up, Down : 행단위 이동
+		// Tap : 들여쓰기
+		// Shift + Tap : 내어쓰기
+		// Ctrl + i : 자동 들여쓰기
+		// Ctrl + / : 주석(토글) -> 한 줄 주석
+			// cf) /* 여러줄 주석 */
+		// Ctrl + Space : 자동완성
+		// Ctrl + F11 : 실행(Run)
+		System.out.println("/*주석아님*/"); // 따옴표 안은 텍스트로 간주되기때문에 주석으로 처리되지 않음
+		System.out.println("sysout");
+		System.out.println("Hotkey");
+		System.out.println("Hotkey");
+		
+	}
+
+}
